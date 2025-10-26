6ce89dcddc4750c364ba9b9522b4596188db40aa
laxs2000 <lax2000@gmail.com>
2025-10-26 19:00:00 +0000

    enhance: Add executive styling with improved visual hierarchy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
+import '../styles/executive.css';
 import Layout from '../components/Layout';
 
 export default function App({ Component, pageProps }: AppProps) {
